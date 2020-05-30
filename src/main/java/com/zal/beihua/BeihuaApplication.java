c1fdaf057d47b9a729586c5338ffd372157c7e47
itjavalx <itjavalx@163.com>
2020-05-30 16:42:30 +0800

    呗花网初始提交。

---
+++ src/main/java/com/zal/beihua/BeihuaApplication.java
@@ -0,0 +1,13 @@
+package com.zal.beihua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeihuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeihuaApplication.class, args);
+	}
+
+}
